3736dadc5015a1226760b7d4f32e16370a09df6d
박상민 <56113148+sangmin802@users.noreply.github.com>
2023-04-21 17:27:27 +0900

    chore(pwa): lostark-hands-next PWA 추가 (#11)
    * chore(pwa): pwa setting
    
    * docs(pwa): config 주석 추가

--- apps/lostark-hands-next/.eslintrc.js
+++ apps/lostark-hands-next/.eslintrc.js
@@ -1,5 +1,6 @@
 /** @type { import("eslint").Linter.Config } */
 module.exports = {
 	root: true,
+	ignorePatterns: ['public/*'],
 	extends: ['@sono-repo/next']
 };
--- apps/lostark-hands-next/next.config.js
+++ apps/lostark-hands-next/next.config.js
@@ -1,9 +1,19 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')({
+	dest: 'public',
+	/** Next13 / PWA bad-precaching-response issue
+	 * @link https://github.com/shadowwalker/next-pwa/issues/424
+	 */
+	buildExcludes: [/app-build-manifest.json$/]
+});
+
+/**
+ * @type {import('next').NextConfig}
+ * @link https://beta.nextjs.org/docs/api-reference/next-config
+ */
+module.exports = withPWA({
 	experimental: {
 		appDir: true
 	},
 	transpilePackages: ['@sono-repo/ui']
-};
-
-module.exports = nextConfig;
+});
