cc0c45c3ad44bee3360c76d0b2c76b14b100284e
sangmin <sangmin802@naver.com>
2023-04-21 17:25:38 +0900

    docs(pwa): config 주석 추가

--- apps/lostark-hands-next/next.config.js
+++ apps/lostark-hands-next/next.config.js
@@ -1,10 +1,16 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')({
 	dest: 'public',
+	/** Next13 / PWA bad-precaching-response issue
+	 * @link https://github.com/shadowwalker/next-pwa/issues/424
+	 */
 	buildExcludes: [/app-build-manifest.json$/]
 });
 
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ * @link https://beta.nextjs.org/docs/api-reference/next-config
+ */
 module.exports = withPWA({
 	experimental: {
 		appDir: true
