2b2c21857d7bfadf1b9985d5170d0848c912e2f7
박상민 <56113148+sangmin802@users.noreply.github.com>
2023-11-15 16:16:48 +0900

    chore: timer 개선 (#83)

--- packages/timer/README.md
+++ packages/timer/README.md
@@ -1,3 +1,9 @@
 ## sono-repo-react-timer
 
-`React` 기반 테스트용 `timer hook`
+`React` 기반 `NPM` 배포 테스트용 `timer hook`
+
+- `endTime`: 종료시간
+- `resetKey`: `timer RAF` 재생성 키
+- `onCallback`: `timer` 종료 핸들러
+- `onWindowFocus`: `window focus event` 핸들러
+  - 브라우저 `focusout` 시, `timer`가 일시중지되는 경우, `reset` 위한 핸들러
--- packages/timer/rollup.config.js
+++ packages/timer/rollup.config.js
@@ -1,27 +1,28 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 const typescript = require('@rollup/plugin-typescript');
 const resolve = require('@rollup/plugin-node-resolve');
 const commonjs = require('@rollup/plugin-commonjs');
 
 /** @type {import('rollup').RollupOptions} */
 module.exports = {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: "./dist/index.mjs",
-      format: 'esm',
-    },
-    {
-      file: "./dist/index.cjs",
-      format: 'cjs',
-    }
-],
-  // react is peerDependency
-  external: ['react'],
-  plugins: [
-    resolve(),
-    commonjs(),
-    typescript({
-      tsconfig: './tsconfig.json'
-    }),
-  ],
+	input: 'src/index.ts',
+	output: [
+		{
+			file: './dist/index.mjs',
+			format: 'esm'
+		},
+		{
+			file: './dist/index.cjs',
+			format: 'cjs'
+		}
+	],
+	// react is peerDependency
+	external: ['react'],
+	plugins: [
+		resolve(),
+		commonjs(),
+		typescript({
+			tsconfig: './tsconfig.json'
+		})
+	]
 };
