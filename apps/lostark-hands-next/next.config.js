2fa0972e1fa6615d14f6e43fcd1fc11675828b8b
박상민 <sangmin802@naver.com>
2023-12-05 10:42:46 +0900

    feat(worker): 모노레포 환경에서 next-pwa의 worker가 읽힐 때, 무한 리빌딩되는 문제가 있어 임시 비활성화

--- apps/lostark-hands-next/next.config.js
+++ apps/lostark-hands-next/next.config.js
@@ -2,18 +2,24 @@
 /**
  * next13 appDir 호환 가능한 next-pwa
  * @link https://github.com/DuCanhGH/next-pwa
+ * 이거 모노레포에서 sw파일 읽을 때 무한 리로딩 문제가 있음.. 다른 일반 레포에 동일하게 올려봤는데, 그 환경은 동일 문제 없음..
+ * 제거...
  */
-const withPWA = require('@ducanh2912/next-pwa').default();
+// const withPWA = require('@ducanh2912/next-pwa').default({
+// 	dest: 'public',
+// 	cacheOnFrontEndNav: true,
+// 	aggressiveFrontEndNavCaching: true
+// });
 
 /**
  * @type {import('next').NextConfig}
  * @link https://beta.nextjs.org/docs/api-reference/next-config
  * 13.4부터 appDir이 기본
  */
-module.exports = withPWA({
-	reactStrictMode: false,
+module.exports = {
+	reactStrictMode: true,
 	images: {
 		domains: ['cdn-lostark.game.onstove.com', 'img.lostark.co.kr']
 	},
 	transpilePackages: ['@sono-repo/ui', '@sono-repo/util']
-});
+};
