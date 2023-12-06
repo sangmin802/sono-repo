521ec35e974a7037efb6f97206fb4a22fbd7745a
박상민 <sangmin802@naver.com>
2023-12-06 14:25:47 +0900

    design(button): button story 추가

--- packages/ui/src/component/button.tsx
+++ packages/ui/src/component/button.tsx
@@ -1,10 +1,19 @@
 import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import cn from 'classnames';
 
 const Button = ({
+	className,
 	children,
 	...props
 }: ButtonHTMLAttributes<HTMLButtonElement> & { children: ReactNode }) => {
-	return <button {...props}>{children}</button>;
+	return (
+		<button
+			className={cn('flex items-center justify-center', className)}
+			{...props}
+		>
+			{children}
+		</button>
+	);
 };
 
 export default Button;
---
+++ packages/ui/src/stories/button.stories.tsx
@@ -0,0 +1,28 @@
+import type { ComponentProps } from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Button } from '../index';
+
+type TButtonProps = ComponentProps<typeof Button>;
+
+type Story = StoryObj<TButtonProps>;
+
+/**
+ * ### Button Component
+ */
+const meta = {
+	title: 'DesignSystem/Component/Button',
+	argTypes: {
+		children: { description: 'Button 컨텐츠' }
+	},
+	args: {
+		children: 'Button'
+	},
+	tags: ['autodocs'],
+	component: Button,
+	render: ({ ...args }) => <Button {...args} />
+} satisfies Meta<TButtonProps>;
+
+export const Default = {} satisfies Story;
+
+export default meta;
