1ffdf498acac9ed30e0afa7bd87407128027aeb2
박상민 <56113148+sangmin802@users.noreply.github.com>
2023-12-13 16:05:42 +0900

    docs(isr): isr 정책 문구 추가 (#99)

--- apps/lostark-hands-next/app/page.tsx
+++ apps/lostark-hands-next/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import { getCalendarApi } from '@/service/game-contents';
 import { calendarSelector } from '@/service/game-contents/selector';
 import { getEventApi, getNoticeApi } from '@/service/news';
@@ -9,8 +10,12 @@ import NotificationButton from '@/client-component/pages/home/notification-butto
 import ProcyonCompassSectionList from '@/client-component/pages/home/procyon-compass-section-list';
 
 /**
- * cache time 5min
+ * cache time 5min isr 주기적으로 재빌드
  * {@link https://nextjs.org/docs/app/api-reference/file-conventions/route-segment-config#revalidate}
+ * {@link https://nextjs.org/docs/app/building-your-application/rendering/server-components#switching-to-dynamic-rendering}
+ * 위 링크의 테이블을 보면 이해 좀 됨. 동적기능의 여부 + 데이터캐시 여부 = 해당 경로를 정적으로 생성할지 동적으로 생성할지
+ * force-cache 유사 ssg
+ * no-store 유사 ssr
  */
 export const revalidate = 300;
 
