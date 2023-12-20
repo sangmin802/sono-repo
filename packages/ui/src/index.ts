ad11b775feb6c7e08f875f4e48a4583fc404c2ce
박상민 <56113148+sangmin802@users.noreply.github.com>
2023-12-20 16:18:19 +0900

    fix(storybook): 스토리북에 tailwindcss가 적용되지 않는 문제 수정 (#102)

--- packages/ui/src/index.ts
+++ packages/ui/src/index.ts
@@ -1,3 +1,5 @@
+import './style/main.css';
+
 export { default as Button } from './component/button';
 export { default as Input } from './component/input';
 export { default as Chip } from './component/chip';
