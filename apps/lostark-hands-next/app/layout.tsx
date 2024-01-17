e0426b1cb0eacb8ceb271231d4bac449fe516817
박상민 <sangmin802@naver.com>
2024-01-17 11:26:02 +0900

    feat: speed-insights 추가

--- apps/lostark-hands-next/app/layout.tsx
+++ apps/lostark-hands-next/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { PropsWithChildren } from 'react';
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 import Head from '@/app/head';
 
@@ -32,6 +33,7 @@ const Layout = ({ children }: PropsWithChildren) => {
 					</ModalProvider>
 				</ReactQueryProvider>
 				<Analytics />
+				<SpeedInsights />
 			</body>
 		</html>
 	);
