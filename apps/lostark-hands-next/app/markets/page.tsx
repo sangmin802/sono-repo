4a41ce2cc078eb884dfbfd2b262c08a781a56060
박상민 <56113148+sangmin802@users.noreply.github.com>
2024-01-24 10:35:14 +0900

    chore: timer 패키지 배포 git action 추가 (#142)

--- apps/lostark-hands-next/app/markets/page.tsx
+++ apps/lostark-hands-next/app/markets/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from 'react';
+
 import ServerWrapper from '@/app/server-wrapper';
 
 import { getOptionsApi } from '@/service/markets';
@@ -9,10 +11,12 @@ export const revalidate = 300;
 const Page = () => {
 	return (
 		<div className="space-y-[16px] px-[16px] pb-[16px]">
-			<ServerWrapper
-				apiPromise={getOptionsApi()}
-				render={(data) => <Markets options={data} />}
-			/>
+			<Suspense>
+				<ServerWrapper
+					apiPromise={getOptionsApi()}
+					render={(data) => <Markets options={data} />}
+				/>
+			</Suspense>
 		</div>
 	);
 };
