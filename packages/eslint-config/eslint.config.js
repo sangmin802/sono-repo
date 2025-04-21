9abdd23be42be2c08e4e481a9a5f894299a06b41
박상민 <bagsangmin@sangmin802.local>
2025-04-21 17:10:06 +0900

    docs: eslint config 주석 추가

--- packages/eslint-config/eslint.config.js
+++ packages/eslint-config/eslint.config.js
@@ -5,6 +5,12 @@ import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 import tseslint from 'typescript-eslint';
 
+/**
+ * config에 그냥 넣는 애들은 최소 { rules: 각 라이브러리 rules } 형식이 유지된 애들임
+ * 이 경우, plugins: { name: 각 라이브러리 rules }가 적용되지 않은 애들은, rules를 커스텀해줄 때, 앞에 라이브러리 이름 구분이 안됨
+ *   - ex) max-len 은 eslint.configs.recommended 관련인데, 얘는 plugins: { name } 이 부여되어있지 않아 전체에서 탐색함
+ */
+
 /** @type { import("eslint").Linter.Config } */
 export default defineConfig([
 	eslint.configs.recommended,
