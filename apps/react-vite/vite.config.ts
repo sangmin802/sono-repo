cb9f43a53222d3091417c66b56b9069ed36fc822
박상민 <bagsangmin@sangmin802.local>
2025-04-22 18:23:11 +0900

    chore: react-vite tailwindcss v4 구성

--- apps/react-vite/vite.config.ts
+++ apps/react-vite/vite.config.ts
@@ -1,3 +1,4 @@
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
@@ -5,7 +6,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig(() => {
 	return {
-		plugins: [react()],
+		plugins: [react(), tailwindcss()],
 		resolve: {
 			alias: {
 				'@': path.resolve(__dirname, 'src')
