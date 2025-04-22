9ec1f4ffe2af84bb959912b2037581167455bcd6
박상민 <bagsangmin@sangmin802.local>
2025-04-22 17:17:01 +0900

    chore: eslint-plugin-next 추가

--- apps/lostark-hands-next/eslint.config.js
+++ apps/lostark-hands-next/eslint.config.js
@@ -1,5 +1,10 @@
+import { flatConfig as nextEslint } from '@next/eslint-plugin-next';
 import { defineConfig } from 'eslint/config';
 
 import sonoRepoTailwindcssEslint from '@sono-repo/eslint-config/tailwindcss';
 
-export default defineConfig([...sonoRepoTailwindcssEslint]);
+export default defineConfig([
+	...sonoRepoTailwindcssEslint,
+	// next recommededRules included
+	nextEslint.coreWebVitals
+]);
