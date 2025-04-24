d6b9f0bd0cc345559cf8f07cf5ec92e786a199a3
박상민 <bagsangmin@sangmin802.local>
2025-04-24 15:45:27 +0900

    test: treeShaking

--- apps/react-vite/src/app.tsx
+++ apps/react-vite/src/app.tsx
@@ -1,7 +1,10 @@
 import PreWarming from './components/pre-warming';
 import ZodTest from './components/zod-test';
+import { test } from './lib';
 
 const App = () => {
+	// treeShaking test
+	test.test2();
 	return (
 		<div>
 			App
