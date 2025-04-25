0b6f754a9f199f2cd7f27f29ee1a63e058429640
sangmin802 <bagsangmin@sangmin802.local>
2025-04-25 11:09:07 +0900

    refactor

--- apps/react-vite/src/components/react-forget/example-two.tsx
+++ apps/react-vite/src/components/react-forget/example-two.tsx
@@ -2,13 +2,16 @@ import { useEffect, useMemo, useState } from 'react';
 
 const ExampleTwo = () => {
 	const [state, setState] = useState({ name: 'test2', count: 0 });
+
 	const memoized = useMemo(() => {
 		console.log('state 변경으로 memo call', state);
 		return { memoName: state.name, key: 'memo' };
 	}, [state]);
+
 	useEffect(() => {
 		console.log('memo로 계산된 값 변경으로 useEffect call', memoized);
 	}, [memoized]);
+
 	return (
 		<div>
 			<button
@@ -22,6 +25,7 @@ const ExampleTwo = () => {
 };
 const Child = ({ data }: { data: { memoName: string; key: string } }) => {
 	console.log('Test2Child rerender', data);
+
 	return <div>test2 child</div>;
 };
 
