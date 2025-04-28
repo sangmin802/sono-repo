1d42d6969690a8504f4b40ad7d13eda7222aaddb
sangmin802 <sangmin802@naver.com>
2025-04-28 14:58:33 +0900

    feat: play-three 프로젝트 추가

---
+++ apps/play-three/src/app.tsx
@@ -0,0 +1,5 @@
+const App = () => {
+	return <div>three</div>;
+};
+
+export default App;
---
+++ apps/play-three/vite.config.ts
@@ -0,0 +1,26 @@
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+	return {
+		plugins: [react(), tailwindcss()],
+		resolve: {
+			alias: {
+				'@': path.resolve(__dirname, 'src')
+			}
+		},
+		build: {
+			minify: false,
+			outDir: './dist'
+		},
+		envDir: path.resolve(__dirname, './.env'),
+		cacheDir: path.resolve(__dirname, './.vite'),
+		server: {
+			port: 8000,
+			host: true
+		}
+	};
+});
