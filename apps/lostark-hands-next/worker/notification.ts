65caebef5fef627397ded2331b160d9a65b9abfc
sangmin802 <sangmin802@naver.com>
2025-05-30 09:30:23 +0900

    docs: jsdoc 수정

--- apps/lostark-hands-next/worker/notification.ts
+++ apps/lostark-hands-next/worker/notification.ts
@@ -8,6 +8,8 @@ class CreateNotification {
 		return Notification.permission;
 	}
 
+	// 객체에 대한 구독을 시도하면 반환되는 listener는, 내가 객체에 대한 값이 변경되었을 때, 호출되는 함수임.
+	// react 기준, rerender를 발생시키는 함수이기 때문에 객체를 변경하는 setter 내부에서 변경 이후, 이 함수를 호출하면 react.state가 변경된것 처럼 rerender가 발생됨
 	onSubscribe = (listener: () => void) => {
 		this.subscribe = listener;
 		return () => {
