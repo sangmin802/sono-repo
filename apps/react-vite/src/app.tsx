894d31ab1b6b5cbe83d211e31ff13c72bf715d24
박상민 <56113148+sangmin802@users.noreply.github.com>
2025-06-26 15:16:28 +0900

    test: throw promise (#202)

--- apps/react-vite/src/app.tsx
+++ apps/react-vite/src/app.tsx
@@ -1,14 +1,28 @@
+import { Suspense } from 'react';
+
 import PreWarming from './components/pre-warming';
 import ReactForget from './components/react-forget';
+import ThrowPromiseTest from './components/throw-promise-test';
 import ZodTest from './components/zod-test';
+
+const Fallback = () => {
+	console.log('서스펜스 Fallback 렌더링');
+
+	return <div>서스펜스</div>;
+};
 
 const App = () => {
 	return (
 		<div>
 			App
-			<PreWarming />
-			<ZodTest />
-			<ReactForget />
+			<Suspense fallback={<Fallback />}>
+				<ThrowPromiseTest />
+			</Suspense>
+			<Suspense fallback={<div>LOADING...</div>}>
+				{/* <PreWarming /> */}
+				{/* <ZodTest /> */}
+				{/* <ReactForget /> */}
+			</Suspense>
 		</div>
 	);
 };
