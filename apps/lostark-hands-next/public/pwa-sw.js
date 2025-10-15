046531d82d3ea028e3ae383f2a73676e53f62ae6
박상민 <56113148+sangmin802@users.noreply.github.com>
2025-10-15 10:52:09 +0900

    test: sw 테스트 (#210)

--- apps/lostark-hands-next/public/pwa-sw.js
+++ apps/lostark-hands-next/public/pwa-sw.js
@@ -1,3 +1,13 @@
+const fetchSomething = async () => {
+	setInterval(() => {
+		self.navigator.setAppBadge(Math.ceil(Math.random() * 10));
+	});
+
+	self.registration.showNotification('ServiceWorker Cookbook', {
+		body: '로아핸즈'
+	});
+};
+
 self.addEventListener('install', async (e) => {
 	console.log('👀 - 설치 완료!');
 	e.waitUntil(self.skipWaiting());
@@ -5,4 +15,6 @@ self.addEventListener('install', async (e) => {
 
 self.addEventListener('activate', () => {
 	console.log('👀 - 활성화 완료!');
+
+	fetchSomething();
 });
