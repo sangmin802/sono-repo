06609ddd65636e3a9241bcfb16a0d8ebf0231113
sangmin802 <sangmin802@naver.com>
2025-12-12 10:22:23 +0900

    feat: api v10 변경점

--- apps/lostark-hands-next/service/armories/_utils.ts
+++ apps/lostark-hands-next/service/armories/_utils.ts
@@ -1,37 +1,9 @@
 import { removeHtmlTag } from '@sono-repo/util/convert';
 
 import { onlyNumber } from '@/utils';
-import {
-	getIndentContent,
-	getItemPartBox,
-	getSingleTextBox
-} from '@/utils/elements';
+import { getItemPartBox, getSingleTextBox } from '@/utils/elements';
 
 import type { TElementUnionArray } from '@/types/element-json';
-
-export const getElixir = (tooltip: TElementUnionArray) => {
-	const elixir = getIndentContent('엘릭서', tooltip);
-
-	return elixir
-		? Object.values(elixir.value.Element_000.contentStr).map(({ contentStr }) =>
-				removeHtmlTag(contentStr.split('</FONT>')[1]).trimStart()
-			)
-		: undefined;
-};
-
-export const getTranscendence = (tooltip: TElementUnionArray) => {
-	const transcendence = getIndentContent('초월', tooltip);
-
-	if (!transcendence) return undefined;
-
-	const [, htmlText] = transcendence.value.Element_000.topStr.split('[초월]');
-	const [grade, total] = removeHtmlTag(htmlText).split('단계');
-
-	return {
-		grade,
-		total
-	};
-};
 
 export const getAdvancedReinforce = (tooltip: TElementUnionArray) => {
 	const key = '[상급 재련]';
